a355c0b5f3ded56e11d5a6b20a23cbd3cfbc04cf
mini <afanamigo@gmail.com>
2025-08-18 17:33:51 +0800

    submit helloworld.go

---
+++ main/Hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// go project is based on package(src folder level)
+// save as java but grace and much more easy, didn't force to use folder package level(folder is package in java)
+// running with cli: go run filename.go or go build, then run the executable file.
+// in this case is, go run Hello.go, Or go build Hello.go, ./Hello
+func main() {
+	fmt.Println("Hello World, Hello Golang! --insider main")
+}
