e51cb5172d6b62b8e599f5dff17042a99cb2aeb1
mini <afanamigo@gmail.com>
2025-10-04 11:30:32 +0800

    submit day3 print all types

---
+++ main/goStudy/Day3/fmtAllType.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"reflect"
+)
+
+func main() {
+
+	// _ 为匿名引用或命名，go编译器不允许无效引用
+
+	// all data type in Go
+	// 总的来说有4类，1. 数字型 整型和浮点 2.布尔类型 3.字符型&字符串类型 4.数组类型
+
+	// 整型&浮点
+	// 整型 数字代表二进制数值所占的长度（默认为cpu字长：cpu一次性存取数据的最大长度）
+	// 默认情况下为cpu位数（32、64），32位cpu一样可以定义64位字长的数据类型
+	var _ int8
+	var _ int16
+	var _ int32
+	var _ int64
+	var _ int
+	// 无符号（unsigned）
+	var _ uint8
+	var _ uint16
+	var _ uint32
+	var _ uint64
+	var _ uint
+	//浮点 Go中没有float关键字（考虑到float浮点类型不能精确表示数字，浮点的位数决定了精度）
+	var _ float32
+	var _ float64
+	var _ complex64
+	var _ complex128
+
+	// 字符串类型，UTF-8编码
+	var _ string
+	_ = "string"
+	str := "str is string"
+	fmt.Println(str)
+
+	// 20013
+	var _str = '中'
+	fmt.Println(_str)
+	// Printf 格式化显示
+	fmt.Printf("%c", _str)
+	fmt.Println()
+	fmt.Println(string(_str))
+
+	// 字符类型
+
+	var _ bool
+
+	_ = true
+	_ = false
+
+	tr := true
+	fs := false
+	fmt.Println("tr is", tr)
+	fmt.Println("fs is", fs)
+	//a = int, & int32?int64
+	var a = 1
+	var _a = "1"
+	var b int32 = 2
+	var c int64 = 3
+	var d = 3.14
+	var e float32 = 3
+	fmt.Println(_a)
+	fmt.Println(reflect.TypeOf(_a))
+	// int
+	fmt.Println(a)
+	fmt.Println(reflect.TypeOf(a))
+	// int32
+	fmt.Println(reflect.TypeOf(b))
+	// int64
+	fmt.Println(reflect.TypeOf(c))
+	// float64 默认长度
+	fmt.Println(reflect.TypeOf(d))
+	// float32
+	fmt.Println(reflect.TypeOf(e))
+
+	// 浮点的比较，不能使用 “==”、“>”、“<”
+	// 使用big包中的函数，-1小于 0等于 1大于
+	f := 3.14
+	g := 3.15
+	result := big.NewFloat(f).Cmp(big.NewFloat(g))
+	fmt.Println(result)
+}
